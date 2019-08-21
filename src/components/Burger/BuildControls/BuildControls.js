18ec4c43af7ec6fde10caa0be0c9be131ef61cb4
Tatiana <tekl@edc.dk>
2019-08-21 15:10:46 +0200

    Adding the Order Button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -21,6 +21,10 @@ const buildControls = (props) => (
                 disabled={props.disabled[ctrl.type]}
             />
        ))}
+       <button 
+            className="OrderButton"
+            disabled={!props.purchaseable}    
+        >ORDER NOW</button>
     </div>
 )
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,6 +18,18 @@ class BurgerBuilder extends Component {
             meat: 0,
         },
         totalPrice: 4,
+        purchaseable: false,
+    }
+
+    updatePurchaseState (ingredients) {
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+            return ingredients[igKey]
+        })
+            .reduce((sum, el) => {
+            return sum + el
+        }, 0)
+        this.setState({purchaseable: sum > 0})
     }
 
     addIngredientHandler = (type) => {
@@ -33,6 +45,8 @@ class BurgerBuilder extends Component {
         const newPrice = oldPrice + priceAddition
 
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+
+        this.updatePurchaseState(updatedIngredients)
     }
 
     removeIngredientHandler = (type) => {
@@ -68,6 +82,7 @@ class BurgerBuilder extends Component {
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
                     price={this.state.totalPrice}
+                    purchaseable={this.state.purchaseable}
                 />
             </Aux>
         )
