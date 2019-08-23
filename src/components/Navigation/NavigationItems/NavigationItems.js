8691dc720b651e5d0949b251a3e9c9fa2d1a26d7
Tatiana <tekl@edc.dk>
2019-08-23 14:56:30 +0200

    Adding Reusable Navigation Items

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NavigationItems.css'
+import NavigationItem from './NavigationItem/NavigationItem'
+
+const navigationItems = (props) => (
+    <ul className="NavigationItems">
+        <NavigationItem link="/" active>Burger Builder</NavigationItem>
+        <NavigationItem link="/">Checkout</NavigationItem>
+    </ul>
+)
+
+export default navigationItems
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Toolbar.css'
 import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const toolbar = (props) => (
     <header className="Toolbar">
         <div>Menu</div>
         <Logo />
         <nav>
-            ...
+            <NavigationItems />
         </nav>
     </header>
 )
