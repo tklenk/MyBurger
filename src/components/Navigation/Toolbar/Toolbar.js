52285e0e3c92ab324389306d8e969bb5e8af1256
Tatiana <tekl@edc.dk>
2019-08-23 11:43:55 +0200

    Using a Logo

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Toolbar.css'
+import Logo from '../../Logo/Logo'
 
 const toolbar = (props) => (
     <header className="Toolbar">
         <div>Menu</div>
-        <div>LOGO</div>
+        <Logo />
         <nav>
             ...
         </nav>
