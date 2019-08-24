bb2580900707bfc86ab46c53bbe15abc15717d38
Tatiana <tekl@edc.dk>
2019-08-24 12:18:16 +0200

    Adding a Hamburger icon instead of Menu

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
+import './DrawerToggle.css'
 
 const drawerToggle = (props) => (
-    <div onClick={props.clicked}>Menu</div>
-)
+    <div className="DrawerToggle" onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+    )
 
 export default drawerToggle
