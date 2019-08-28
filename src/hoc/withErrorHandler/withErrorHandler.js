f2c2b364e51c5b95ceab9ff48f3b97cde1d95690
Tatiana <tekl@edc.dk>
2019-08-28 08:54:45 +0200

    Error Handling1

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Modal from '../../components/UI/Modal/Modal'
+import Aux from '../Auxil/Auxil'
+
+const withErrorHandler = (WrappedComponent) => {
+    return (props) => {
+        return (
+            <Aux>
+                <Modal show>
+                    Something didn't work!
+                </Modal>
+                <WrappedComponent {...props} />
+            </Aux>
+        )
+    }
+}
+
+export default withErrorHandler
