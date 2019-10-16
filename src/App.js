b4bacd1f11ece9e44f52553d822665ede3791853
Tatiana <tekl@edc.dk>
2019-10-16 13:38:21 +0200

    clean up

--- src/App.js
+++ src/App.js
@@ -3,26 +3,16 @@ import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
-  //state and componentDidMount to check if componentWillUnmount (eject) works in withErrorHandling
-  state = {
-    show: true 
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({show: false})
-    }, 5000)
-  }
 
   render () {
     return (
       <div>
         <Layout>
-          {this.state.show ? <BurgerBuilder/> : null}
+          <BurgerBuilder/>
         </Layout>
       </div>
     );
-    }
+  }
 }
 
 export default App;
