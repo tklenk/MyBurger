be4471aaa840b8adcb12f1e98ee5f015a38f36b5
Tatiana <tekl@edc.dk>
2019-10-16 13:37:04 +0200

    Removing old interceptors

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
+  //state and componentDidMount to check if componentWillUnmount (eject) works in withErrorHandling
+  state = {
+    show: true 
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({show: false})
+    }, 5000)
+  }
+
   render () {
     return (
       <div>
         <Layout>
-          <BurgerBuilder/>
+          {this.state.show ? <BurgerBuilder/> : null}
         </Layout>
       </div>
     );
