e124d5992834466c49e4316007de6dc6f1fcc293
Tanya Kl <tatiana.klenk@gmail.com>
2020-01-21 12:14:35 +0100

    withRoute as HOC - example

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
+
 import './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = ( props ) => {
+    console.log(props)
     let transformedIngredients = Object.keys( props.ingredients )
         .map(igKey => {
             return [...Array( props.ingredients[igKey] )].map((_, i) => {
@@ -24,4 +27,4 @@ const burger = ( props ) => {
     )
 }
 
-export default burger
+export default withRouter(burger)
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -32,6 +32,7 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount() {
+        console.log(this.props)
         axios.get('https://react-my-burger-e6a56.firebaseio.com/ingredients.json')
             .then(response => {
                 this.setState({ingredients: response.data})
@@ -93,28 +94,28 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue!');
-        this.setState( { loading: true } );
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Lila Nowak',
-                address: {
-                    street: 'Teststreet 1',
-                    zipCode: '90898',
-                    country: 'Poland'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post( '/orders.json', order )
-            .then( response => {
-                this.setState( { loading: false, purchasing: false } );
-            } )
-            .catch( error => {
-                this.setState( { loading: false, purchasing: false } );
-            } );
+        // this.setState( { loading: true } );
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Lila Nowak',
+        //         address: {
+        //             street: 'Teststreet 1',
+        //             zipCode: '90898',
+        //             country: 'Poland'
+        //         },
+        //         email: 'test@test.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post( '/orders.json', order )
+        //     .then( response => {
+        //         this.setState( { loading: false, purchasing: false } );
+        //     } )
+        //     .catch( error => {
+        //         this.setState( { loading: false, purchasing: false } );
+        //     } );
     }
 
     render () {
