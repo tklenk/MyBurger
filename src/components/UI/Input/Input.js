08e2407c614b328ed6bd8d747ce1ee80b70e0f90
Tanya Kl <tatiana.klenk@gmail.com>
2020-01-24 10:19:36 +0100

    Create select input dynamically

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -7,10 +7,29 @@ const input = (props) => {
 
     switch(props.elementType) {
         case ('input'):
-            inputElement = <input className="InputElement" {...props.elementConfig} value={props.value} />
+            inputElement = <input 
+                className="InputElement" 
+                {...props.elementConfig} 
+                value={props.value} />
             break
         case ('textarea'):
-            inputElement = <textarea className="InputElement" {...props.elementConfig} value={props.value} />
+            inputElement = <textarea
+                className="InputElement" 
+                {...props.elementConfig} 
+                value={props.value} />
+            break
+        case ('select'):
+            inputElement = (
+                <select 
+                    className="InputElement"  
+                    value={props.value}>
+                    {props.elementConfig.options.map(option => (
+                        <option key={option.value} value={option.value}>
+                            {option.displayValue}
+                        </option>
+                    ))}    
+                </select>
+            )
             break
         default:
             inputElement = <input className="InputElement" {...props.elementConfig} value={props.value} />
