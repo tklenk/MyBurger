7b12ffa00011b228d3513a7be67c2876dad9962c
Tanya Kl <tatiana.klenk@gmail.com>
2020-01-29 15:42:13 +0100

    Working on the total price calculation

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,2 +1,33 @@
 export const ADD_INGREDIENT = 'ADD_INGREDIENT'
 export const REMOVE_INGREDIENT = 'REMOVE_INGREDIENT'
+
+// addIngredientHandler = ( type ) => {
+//     const oldCount = this.state.ingredients[type];
+//     const updatedCount = oldCount + 1;
+//     const updatedIngredients = {
+//         ...this.state.ingredients
+//     };
+//     updatedIngredients[type] = updatedCount;
+//     const priceAddition = INGREDIENT_PRICES[type];
+//     const oldPrice = this.state.totalPrice;
+//     const newPrice = oldPrice + priceAddition;
+//     this.setState( { totalPrice: newPrice, ingredients: updatedIngredients } );
+//     this.updatePurchaseState( updatedIngredients );
+// }
+
+// removeIngredientHandler = ( type ) => {
+//     const oldCount = this.state.ingredients[type];
+//     if ( oldCount <= 0 ) {
+//         return;
+//     }
+//     const updatedCount = oldCount - 1;
+//     const updatedIngredients = {
+//         ...this.state.ingredients
+//     };
+//     updatedIngredients[type] = updatedCount;
+//     const priceDeduction = INGREDIENT_PRICES[type];
+//     const oldPrice = this.state.totalPrice;
+//     const newPrice = oldPrice - priceDeduction;
+//     this.setState( { totalPrice: newPrice, ingredients: updatedIngredients } );
+//     this.updatePurchaseState( updatedIngredients );
+// }
